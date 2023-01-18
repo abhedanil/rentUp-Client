d33f6db2fda52d4e14a6ac6d464b93d639b4f136
Abhed Anil <abhedanil@gmail.com>
2023-01-18 17:39:11 +0530

    commit 18th jan evening

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,17 +1,11 @@
 
 import React from 'react'
 import NavBar from '../components/Navbar'
-import axios from 'axios';
+
 
 import { Box, TextField, Divider, Typography, Button, CardMedia, CardContent, CardActions, Card } from '@mui/material'
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { Button } from '@mui/material';
+
 
 
 function UserAccount() {
@@ -32,11 +26,7 @@ function UserAccount() {
     };
 
 
-    const [value, setValue] = React.useState('1');
-    const [Items, setItems] = useState()
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  
 
     return (
         <>
@@ -67,43 +57,7 @@ function UserAccount() {
                         <Card sx={{ width: 700, height: "500px" }} >
                             <Divider sx={{ mt: 3 }}>My bookings</Divider>
                             <CardContent>
-                                <TableContainer sx={{ ml: 0, mb: 3 }} component={Paper} >
-                                    <Table sx={{ minWidth: 550 }} size="small" aria-label="a dense table">
-                                        <TableHead>
-
-                                        </TableHead>
-                                        {Items ? (
-
-                                            <>
-                                                <TableBody >
-                                                    {Items?.map((car) => (
-                                                        <TableRow
-                                                            key={car.carname}
-                                                        >
-                                                            <TableCell sx={{ width: "250px" }} >
-                                                                <img src={car.carimage} style={{ width: "150px", height: "100px" }} />
-                                                            </TableCell>
-                                                            <TableCell align="left" sx={{ mr: "5" }} >
-                                                                <Typography variant="h5">{car.companyName} {car.carname}</Typography>
-                                                                <Typography sx={{ mt: 1 }} > <EventSeatIcon sx={{ width: "17px" }} />  {car.seatCapacity}seater    <LocalGasStationIcon sx={{ width: "17px" }} /> {car.fueltype}    <SpeedIcon sx={{ width: "17px" }} />  {car.transmission}</Typography>
-                                                            </TableCell>
-                                                            {/*<TableCell align="center" ></TableCell>*/}
-                                                            <TableCell align="center">{car.location} | {car.district}</TableCell>
-                                                            <TableCell align="center" ><Typography sx={{ color: "black", fontWeight: "bold" }} variant="h6">{car.rentPerDay}₹</Typography></TableCell>
-                                                            <TableCell align="center" ><Button variant="contained" color="success"><Link to={`/booking/${car._id}`}>Book now</Link></Button></TableCell>
-                                                        </TableRow>
-                                                    ))}
-                                                </TableBody>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <Typography>Nothing to show</Typography>
-                                            </>
-                                        )}
-
-                                    </Table>
-                                </TableContainer>
-
+                                
                             </CardContent>
 
                             <CardActions sx={{ paddingTop: "17 0px", paddingLeft: "180px" }} size="small" align="center">
